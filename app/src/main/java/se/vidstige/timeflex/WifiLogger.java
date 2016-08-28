669267475cc024c2ec6b1c9357b2ad4104182c97
Samuel Carlsson <samuel.carlsson@gmail.com>
2016-08-28 19:36:19 +0200

    Removing some autogenerated crap.

--- app/src/main/java/se/vidstige/timeflex/WifiLogger.java
+++ app/src/main/java/se/vidstige/timeflex/WifiLogger.java
@@ -7,6 +7,11 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.util.Log;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by vidstige on 23/08/16.
  */
@@ -15,13 +20,34 @@ public class WifiLogger extends BroadcastReceiver {
         Log.i("WiFiLogger", "Created");
     }
 
+    private String createFilename(int i) {
+        return String.format("%05d.scan", i);
+    }
+
+    private String getFilename(Context context) {
+        List<String> files = Arrays.asList(context.fileList());
+        String filename;
+        int i = 0 ;
+        do {
+            filename = createFilename(i);
+            i++;
+        } while (files.contains(filename));
+        return filename;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.i("WiFiLogger", "Scan results received");
         WifiManager w = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
-        Log.i("WiFiLogger", "SSIDs:");
-        for (ScanResult r : w.getScanResults()) {
-            Log.i("WiFiLogger", "SSID: " + r.SSID + ", level: " + r.level);
-            //a.setText(r.SSID + "" + r.level + "\r\n");
+        try {
+            PrintWriter scan_results = new PrintWriter(context.openFileOutput(getFilename(context), Context.MODE_PRIVATE));
+            for (ScanResult r : w.getScanResults()) {
+                scan_results.println(r.SSID + ":" + r.level);
+            }
+            scan_results.close();
+            Log.i("WiFiLogger", "...file written");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
